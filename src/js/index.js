8cc6d8aa4bd42fe75ad9fe5fbafee70463938d8d
Jaehoon Lee <jaehoonlee@Jaehoons-MacBook-Pro.local>
2021-02-04 10:27:20 +0900

    refactor: reorganize the time.js index.js and fetchBackgroundImage.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,16 @@
-
-
-
 import weatherHandler from './weather';
 import { getBackgroundImage } from './fetchBackgroundImage';
-import { isAlreadyVisited, checkTodayDate, displayCurrentTime, setTodayDate } from './timer';
-import { mainTodoHandler, removeMainTodo, toggleMainTodo } from './mainTodo';
+import { displayCurrentTime, setTodayDate } from './time';
+import { mainTodoHandler} from './mainTodo';
 import { ACCESS_KEY } from './utils/constants';
 
-if (!isAlreadyVisited() || checkTodayDate()) {
-    setTodayDate();
-    // getBackgroundImage(ACCESS_KEY);
-}
 
-weatherHandler();
+getBackgroundImage(ACCESS_KEY);
+
+setTodayDate();
 displayCurrentTime();
 setInterval(displayCurrentTime, 1000);
+
+weatherHandler();
 
 mainTodoHandler();
